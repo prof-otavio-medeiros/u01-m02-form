e6b2cf8077e209f0cc94dfd581873c16ba90b9fc
Otávio Soares Cavalcante de Medeiros <otavio.medeiros@dataprev.gov.br>
2022-09-20 19:31:05 -0300

    commit 2022 09 20 001

--- scripts/form-input.js
+++ scripts/form-input.js
@@ -1,6 +1,18 @@
-function updateQuantityRangeStep(idBase, value, decimals = 0) {
-  document.getElementById(`${idBase}-value`).innerText = "Valor: " + parseFloat(value).toFixed(decimals);
+function showHiddenValue() {
+  alert(document.getElementById("custId").value);
 }
 
-updateQuantityRangeStep("quantity-range", document.getElementById("quantity-range").value);
-updateQuantityRangeStep("quantity-range-step", document.getElementById("quantity-range-step").value, 4);
+function updateQuantityRangeStep(idBase, value, decimals = 0) {
+  document.getElementById(`${idBase}-value`).innerText =
+    "Valor: " + parseFloat(value).toFixed(decimals);
+}
+
+updateQuantityRangeStep(
+  "quantity-range",
+  document.getElementById("quantity-range").value
+);
+updateQuantityRangeStep(
+  "quantity-range-step",
+  document.getElementById("quantity-range-step").value,
+  4
+);
